ee50c342d47675f3418c5cd0e39dc4d78ad5ce42
Mark Solberger <mountain_dude@hotmail.com>
2014-12-20 20:44:17 +0000

    commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
+var txt = Buffer.toString(fs.readFileSync('index.html'))
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(txt)
 })
 
 app.listen(app.get('port'), function() {
