9635cb393b13b892762bd2d8cc09f1bed6f4e711
Mark Solberger <mountain_dude@hotmail.com>
2014-12-20 20:46:54 +0000

    commit2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 var fs = require('fs')
-var txt = Buffer.toString(fs.readFileSync('index.html'))
+var txt = new Buffer(fs.readFileSync('index.html')).toString()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
